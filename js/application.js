124535b6d0786db44678b18eb75fcf5de0cfaea2
Paulo Clavijo Esteban <paulo.esteban@emc.com>
2014-05-23 16:02:53 +0100

    Replacing fixture DS adapter with local store DS adapter

--- js/application.js
+++ js/application.js
@@ -5,4 +5,8 @@ window.Todos = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+//Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
+});
