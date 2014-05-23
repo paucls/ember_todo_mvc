c57184819eec3c2ac03feed0cde24315a6bff457
Paulo Clavijo Esteban <paulo.esteban@emc.com>
2014-05-23 14:51:35 +0100

    implement show only incomplete todos

--- js/router.js
+++ js/router.js
@@ -4,6 +4,7 @@
 Todos.Router.map(function() {
     this.resource('todos', { path: '/' }, function() {
         // additional child routes
+        this.route('active');
     });
 });
 
@@ -17,4 +18,16 @@ Todos.TodosIndexRoute = Ember.Route.extend({
     model: function() {
         return this.modelFor('todos');
     }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+   model: function() {
+       return this.store.filter('todo', function(todo) {
+           return !todo.get('isCompleted');
+       });
+   },
+
+   renderTemplate: function(controller) {
+       this.render('todos/index', {controller: controller});
+   }
 });
